a1bb640fd422fbd6408b65ffa02d1c564166cdfb
Greg <gskril@gmail.com>
2022-11-24 22:25:03 -0500

    Upload and pin image from frontend to IPFS

--- src/pages/api/pin.ts
+++ src/pages/api/pin.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import pinataSDK, { PinataPinResponse } from '@pinata/sdk'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
@@ -10,13 +11,25 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<void | PinataPinResponse>
 ) {
-  const src = 'public/favicon.ico'
-  const options = {
-    pinataMetadata: {
-      name: 'favicon',
-    },
-  }
+  const data = JSON.parse(req.body)
+  const { image, filename } = data
 
-  const response = await pinata.pinFromFS(src, options)
+  // Decode the base64 string
+  const imageBuffer = Buffer.from(image, 'base64')
+
+  // Save the image to disk
+  const filePath = `public/gen/${filename}`
+  fs.writeFileSync(filePath, imageBuffer)
+
+  // Pin the image to IPFS
+  const response = await pinata.pinFromFS(filePath, {
+    pinataMetadata: {
+      name: filename,
+    },
+  })
+
   res.status(200).json(response)
+
+  // Delete the image from disk
+  fs.rmSync(filePath)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,33 @@
+import { FormEvent } from 'react'
+
 export default function Home() {
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    const image = e.currentTarget.file.files[0]
+    const imageBuffer = await image.arrayBuffer()
+
+    await fetch('/api/pin', {
+      method: 'POST',
+      body: JSON.stringify({
+        image: Buffer.from(imageBuffer).toString('base64'),
+        filename: image.name,
+      }),
+    })
+  }
+
   return (
     <div>
-      <form>
-        <input type="file" accept="image/png, image/jpeg" />
+      <form onSubmit={async (e) => await handleSubmit(e)}>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          accept="image/png, image/jpeg"
+          required
+        />
+        <br />
+        <button type="submit">Upload</button>
       </form>
     </div>
   )
